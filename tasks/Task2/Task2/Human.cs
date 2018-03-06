e2a0914baf4381c35d5f474104d7d9f8d8c779f6
unknown <ic17b022@technikum-wien.at>
2018-03-06 17:44:22 +0100

    Task2

---
+++ tasks/Task2/Task2/Human.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Human
+    {
+        private int Size;
+        public string FirstName { get; }
+        public string LastName { get; set; }
+        public int Weight { get; set; }
+
+        public Human(int size, string firstName, string lastName, int weight)
+        {
+            Size = size;
+            FirstName = firstName;
+            LastName = lastName;
+            Weight = weight;
+        }
+
+        public void updateSize (int size)
+        {
+            if (size >= Size)
+                Size = size;
+            else
+                throw new ArgumentOutOfRangeException("Humans don't shrink in this abstraction! Your size " + size.ToString() + " is smaller than current size " + Size.ToString());
+        }
+
+        override public string ToString()
+        {
+            return Size.ToString() + " " + FirstName + " " + LastName + " " + Weight;
+        }
+
+    }
+}
---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human1 = new Human(182, "Hans", "Gruber", 85);
+            Human human2 = new Human(160, "Hanna", "Gruber", 50);
+
+            Console.WriteLine(human1.ToString());
+
+            Console.WriteLine("human2 before change: " + human2.Weight);
+            human2.updateSize(162);
+            Console.WriteLine("human2 after change: " + human2.Weight);
+
+        }
+    }
+}
