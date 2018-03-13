4371a648da0e76770921727a123d42c55bfb476a
unknown <ic17b022@technikum-wien.at>
2018-03-13 17:57:03 +0100

    Task 3

---
+++ tasks/Task3/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICreature[] creatures = new ICreature[]
+            {
+                new Human(178, "Hans", "Huber", 80),
+                new Lizard(15, 0.2M),
+            };
+   
+            foreach(ICreature x in creatures){
+                Console.WriteLine(x.ToString());
+            }
+        }
+    }
+}
