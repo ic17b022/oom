375dedd905cc1c82cb17bebaf16ab12f7c68c1a4
unknown <ic17b022@technikum-wien.at>
2018-04-17 10:05:56 +0200

    implement Task6

---
+++ tasks/Task6/Task6/Human.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task6
+{
+    class Human : ICreature
+    {
+        public int Size { get; private set; }
+        public string FirstName { get; }
+        public string LastName { get; set; }
+        public int Weight { get; set; }
+
+        public Human(int size, string firstName, string lastName, int weight)
+        {
+            Size = size;
+            FirstName = firstName;
+            LastName = lastName;
+            Weight = weight;
+        }
+
+        public void updateSize(int size)
+        {
+            if (size >= Size)
+                Size = size;
+            else
+                throw new ArgumentOutOfRangeException("Humans don't shrink in this abstraction! Your size " + size.ToString() + " is smaller than current size " + Size.ToString());
+        }
+
+        override public string ToString()
+        {
+            return String.Format("{0}, {1}, {2}, {3}, {4}", Size.ToString(), FirstName, LastName, Weight.ToString(), isWarmblooded());
+        }
+
+        public bool isWarmblooded()
+        {
+            return true;
+        }
+    }
+}
---
+++ tasks/Task6/Task6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Subject<Human> subject = new Subject<Human>();
+            List<Human> list1 = new List<Human> { new Human(180, "Hans", "Maier", 80), new Human(160, "Gretel", "Maier", 50) };
+            List<Human> list2 = new List<Human>();
+
+
+            subject.Subscribe<Human>(x => list2.Add(x));
+
+            for (int i = 0; i < 2; i++)
+            {
+
+                subject.OnNext(list1[i]);
+                Console.WriteLine(list2[i].ToString());
+            }
+
+            Console.WriteLine("We found a super complicated way to copy a list! YAY!!");
+
+            for (int i = 10; i > 0; i--)
+            {
+                int counter = i;
+                Task<string> spinOff = Task.Run(() => longAndHardWork(counter*1000));
+                spinOff.ContinueWith(t => Console.WriteLine(t.Result));
+            }
+
+            //wait for sleepyheads to finish
+            System.Threading.Thread.Sleep(15000);
+        }
+
+        private static string longAndHardWork(int duration)
+        {
+            Console.WriteLine("received:" + duration);
+            System.Threading.Thread.Sleep(duration);    //This produces the expected output: Task finish randomly, repeatet runs yield different results
+            //System.Threading.Thread.Sleep(5000);      //This does not. A constant sleep timer will produce the same order every time. Why?
+            return "I twiddled my thumbs for " + duration + " milliseconds!";
+        }
+
+    } 
+}
